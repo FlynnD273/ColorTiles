9a86a03c4c0731d8a9da6f595ee875cda188bce8
Flynn Duniho <flynn@harveyosity.org>
2023-06-28 16:02:34 -0700

    modify zip bash file

--- zip.sh
+++ zip.sh
@@ -5,3 +5,9 @@ cd build/windows
 
 cd ../linux
 7z a -mx=9 colortiles-linux.zip colortiles.x86_64
+
+cd ../
+
+mkdir gitrelease
+mv windows/colortiles-windows.zip gitrelease/
+mv linux/colortiles-linux.zip gitrelease/
